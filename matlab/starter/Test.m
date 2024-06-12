3a47a45cc887182d4782615828ccb91255a4aa4a
Ferdinand Ade <ferdinand.ade@otto.de>
2024-06-12 10:30:08 +0200

    add passing matlab test

--- matlab/starter/Test.m
+++ matlab/starter/Test.m
@@ -1,9 +1,16 @@
-classdef TestGildedRose < matlab.unittest.TestCase
+classdef StarterTest < matlab.unittest.TestCase
 
     methods(Test)
         % Test methods
         function should_not_pass(tc)
             tc.verifyEqual("Food", "Drink")
+        end
+    end
+
+    methods(Test)
+        % Test methods
+        function should_pass(tc)
+            tc.verifyEqual("Drink", "Drink")
         end
     end
 end
